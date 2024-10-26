25080d8588aafd0fb51bae235b01e0ed1a77ee2c
Grasu23 <haisanmatei@gmail.com>
2024-10-26 20:47:28 +0300

    commit modificat main.cpp pt milestone 1

--- main.cpp
+++ main.cpp
@@ -1,57 +1,105 @@
 #include <iostream>
-#include <array>
 
-#include <Helper.h>
+class Player{
+private:
+  short HealthPoints;
+  std::string PlayerName;
+  short PlayerLevel;
+public:
 
-int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
-    return 0;
+  Player()=default;
+  ~Player()=default;
+
+  Player(short HealthPoints,const std::string& PlayerName,short PlayerLevel){
+    this->HealthPoints=HealthPoints;
+    this->PlayerLevel=PlayerLevel;
+    this->PlayerName=PlayerName;
+  }
+
+  Player& operator=(const Player& player){
+    this->PlayerName=player.PlayerName;
+    this->PlayerLevel=player.PlayerLevel;
+    this->HealthPoints=player.HealthPoints;
+
+    return *this;
+  }
+
+  Player(const Player& obj){
+    this->HealthPoints=obj.HealthPoints;
+    this->PlayerLevel=obj.PlayerLevel;
+    this->PlayerName=obj.PlayerName;
+  }
+
+  friend std::ostream & operator<<(std::ostream &out,const Player& player){
+    out<<"Player name: "<<player.PlayerName<<'\n';
+    out<<"Player level: "<<player.PlayerLevel<<'\n';
+    out<<"Player HP: "<<player.HealthPoints<<'\n';
+
+    return out;
+  }
+
+
+};
+
+class Enemy{
+private:
+  short HealthPoints;
+  std::string EnemyName;
+  short EnemyLevel;
+  unsigned short EnemySpeed;
+
+public:
+
+  Enemy()=default;
+  ~Enemy()=default;
+
+  Enemy(const std::string& EnemyName,short HealthPoints,short EnemyLevel,unsigned short EnemySpeed){
+    this->EnemyName=EnemyName;
+    this->EnemyLevel=EnemyLevel;
+    this->HealthPoints=HealthPoints;
+    this->EnemySpeed=EnemySpeed;
+  }
+
+  Enemy(const Enemy& obj){
+    this->EnemyName=obj.EnemyName;
+    this->EnemyLevel=obj.EnemyLevel;
+    this->HealthPoints=obj.HealthPoints;
+    this->EnemySpeed=obj.EnemySpeed;
+  }
+
+  friend std::ostream& operator<<(std::ostream& out,const Enemy& enemy){
+    out<<"Enemy name: "<<enemy.EnemyName<<'\n';
+    out<<"Enemy level: "<<enemy.EnemyLevel<<'\n';
+    out<<"Enemy HP: "<<enemy.HealthPoints<<'\n';
+    out<<"Enemy speed: "<<enemy.EnemySpeed<<'\n';
+
+    return out;
+  }
+
+};
+
+class Projectile{
+private:
+  std::string ProjectileType;
+  unsigned int ProjectileSpeed;
+  unsigned int ProjectileDamage;
+public:
+
+  friend std::ostream& operator <<(std::ostream& out, const Projectile& projectile){
+    out<<"Projectile type: "<<projectile.ProjectileType<<'\n';
+    out<<"Projectile damage: "<<projectile.ProjectileDamage<<'\n';
+    out<<"Projectile speed: "<<projectile.ProjectileType<<'\n';
+
+    return out;
+  }
+
+};
+
+class Menu{
+};
+
+int main(){
+  return 0;
 }
+
+
