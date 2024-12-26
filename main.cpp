b38bad4e5dc0bcc89748dd1dd59b48e9c007e257
Grasu23 <haisanmatei@gmail.com>
2024-12-26 08:52:48 +0200

    asdf

--- main.cpp
+++ main.cpp
@@ -12,43 +12,7 @@ int main() {
   int* y = new int;
   float* m = new float;
 
-  try {
-dims:
-
-    std::cout << "Render width : ";
-    std::cin >> *x;
-
-    std::cout << "Render height : ";
-    std::cin >> *y;
-
-    if ( *x < *y && *x > 0 && *y > 0 ) throw NonLandscapeDimensionsException ( );
-
-    if ( *x <= 0 || *y <= 0 )throw NullNegativeDimensionsException ( );
-
-mult:
-
-    std::cout << "Multiplier [1.5 - 2.5] : ";
-    std::cin >> *m;
-
-    if ( *m < 1.5 || *m > 2.5 ) throw InvalidMultiplierException ( );
-  }
-  catch ( const NonLandscapeDimensionsException & err ) {
-    std::cout << err.what ( );
-    goto dims;
-  } catch ( const NullNegativeDimensionsException & err ) {
-    std::cout << err.what ( );
-    goto dims;
-  } catch ( const InvalidMultiplierException & err ) {
-    std::cout << err.what ( );
-    goto mult;
-  }
-
-  DefaultParameters::GetInstance ( )->SetRenderDimensions ( *x, *y );
-  DefaultParameters::GetInstance ( )->SetMultiplier ( *m );
-
-  delete x, delete y, delete m;
-
-  std::shared_ptr<Player> p = std::make_shared<Player> ( new Player ( "Gigel",
+  std::shared_ptr<Player> p = std::make_shared<Player> ( Player ( "Gigel",
                                                                   1,
                                                                   5,
                                                                   0,
@@ -62,7 +26,6 @@ mult:
                                                                   3 ) );
 
   std::unique_ptr<Menu> menu = std::make_unique<Menu> ( PLAYING );
-
 
   std::vector<std::shared_ptr<NonPlayer>> nps;
 
